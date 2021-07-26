5ae0f7c6a941ab36dae11e1f55f184df1140cbf1
Ygor Mendes <ygortmendes@gmail.com>
2021-07-26 15:38:08 -0300

    change z-index of the Modal

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    zIndex: {
+      0: 0,
+      10: 10,
+      20: 20,
+      30: 30,
+      40: 40,
+      50: 50,
+      60: 60,
+      70: 70,
+      80: 80,
+      90: 90,
+      100: 100,
+      auto: 'auto',
+    },
     extend: {
       transitionDuration: {
         0: '0ms',
